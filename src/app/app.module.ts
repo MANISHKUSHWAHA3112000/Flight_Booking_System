1057de233883f5c9060a3f251cc155aaa8bc657b
Manish Kushwaha <kushwahamanish311@gmail.com>
2025-09-19 00:43:39 +0530

    added header and footer

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  imports: [BrowserModule, MaterialModule]
+})
+export class AppModule { }
